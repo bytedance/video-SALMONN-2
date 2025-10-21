efa89187864006a5c5cc31040b70f8886a5fb581
Yixuan Li <32223554+DragonAura@users.noreply.github.com>
2025-10-21 13:39:01 +0800

    Uncomment LayerNorm initialization in modeling_bert.py

--- video_SALMONN2_plus/qwenvl/model/modeling_bert.py
+++ video_SALMONN2_plus/qwenvl/model/modeling_bert.py
@@ -664,9 +664,9 @@ class BertPreTrainedModel(PreTrainedModel):
             # Slightly different from the TF version which uses truncated_normal for initialization
             # cf https://github.com/pytorch/pytorch/pull/5617
             module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
-        # elif isinstance(module, nn.LayerNorm):
-        #     module.bias.data.zero_()
-        #     module.weight.data.fill_(1.0)
+        elif isinstance(module, nn.LayerNorm):
+            module.bias.data.zero_()
+            module.weight.data.fill_(1.0)
         #     print(module)
         if isinstance(module, nn.Linear) and module.bias is not None:
             module.bias.data.zero_()
